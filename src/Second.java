eab263656654941eca7bd5145f4196cd54e25254
KommidiVamshiReddy83 <vamshikrishna2588@gmail.com>
2025-03-27 11:09:39 +0530

    Update Second.java

--- src/Second.java
+++ src/Second.java
@@ -1,5 +1,5 @@
 public class Second {
     public static void main(String[] args) {
-        System.out.println("greet the team");
+        System.out.println("greet the hyper grid dev team");
     }
 }
